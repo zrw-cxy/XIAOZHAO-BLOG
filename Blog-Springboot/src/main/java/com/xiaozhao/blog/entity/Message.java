400ac1cd62e7513fbdb6cf9683d1c17b2ef4e6e7
Zhao Ru Wei <934566509@qq.com>
2022-05-16 20:28:38 +0800

    留言

---
+++ Blog-Springboot/src/main/java/com/xiaozhao/blog/entity/Message.java
@@ -0,0 +1,77 @@
+package com.xiaozhao.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: XIAO ZHAO
+ * @Date: 2022/5/16 20:15
+ * 留言
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("tb_message")
+public class Message {
+
+    /**
+     * 主键id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户ip
+     */
+    private String ipAddress;
+
+    /**
+     * 用户地址
+     */
+    private String ipSource;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * 头像
+     */
+    private String avatar;
+
+    /**
+     * 留言内容
+     */
+    private String messageContent;
+
+    /**
+     * 弹幕速度
+     */
+    private Integer time;
+
+    /**
+     * 是否审核
+     */
+    private Integer isReview;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+
+}
