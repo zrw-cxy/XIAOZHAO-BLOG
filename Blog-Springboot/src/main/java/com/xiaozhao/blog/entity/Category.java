b70abc76891ae4e61f626fbab90ffcdc8cfd97cb
Zhao Ru Wei <934566509@qq.com>
2022-05-16 18:46:22 +0800

    分类

---
+++ Blog-Springboot/src/main/java/com/xiaozhao/blog/entity/Category.java
@@ -0,0 +1,47 @@
+package com.xiaozhao.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: XIAO ZHAO
+ * @Date: 2022/5/16 18:45
+ * 分类
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("tb_category")
+public class Category {
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 分类名
+     */
+    private String categoryName;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+
+}
