77526858c14f7347196f9a2234b2c7a660c82316
Zhao Ru Wei <934566509@qq.com>
2022-05-16 20:29:30 +0800

    相册

---
+++ Blog-Springboot/src/main/java/com/xiaozhao/blog/entity/PhotoAlbum.java
@@ -0,0 +1,68 @@
+package com.xiaozhao.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: XIAO ZHAO
+ * @Date: 2022/5/16 20:18
+ * 相册
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName(value ="tb_photo_album")
+public class PhotoAlbum  {
+
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 相册名
+     */
+    private String albumName;
+
+    /**
+     * 相册描述
+     */
+    private String albumDesc;
+
+    /**
+     * 相册封面
+     */
+    private String albumCover;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDelete;
+
+    /**
+     * 状态值 1公开 2私密
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+
+
+}
