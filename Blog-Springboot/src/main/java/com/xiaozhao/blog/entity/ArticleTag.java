841ceae91f6ded7e3107981e685699c2f0b118b3
Zhao Ru Wei <934566509@qq.com>
2022-05-16 18:45:09 +0800

    文章标签

---
+++ Blog-Springboot/src/main/java/com/xiaozhao/blog/entity/ArticleTag.java
@@ -0,0 +1,41 @@
+package com.xiaozhao.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: XIAO ZHAO
+ * @Date: 2022/5/16 18:42
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("tb_article_tag")
+public class ArticleTag {
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 文章id
+     */
+    private Integer articleId;
+
+    /**
+     * 标签id
+     */
+    private Integer tagId;
+
+
+}
+
